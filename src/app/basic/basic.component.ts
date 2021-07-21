9348d136b121bd9915e7196e16caefdc3eb26d9e
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2021-07-21 19:36:54 +0530

    check update

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -1,5 +1,6 @@
 import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import AgoraRTC from 'agora-rtc-sdk-ng';
 import { ApiService } from '../services/api.service';
 import { StreamService } from '../services/stream.service';
 
@@ -18,7 +19,11 @@ export class BasicComponent implements OnInit {
   ngOnInit() {
 
   }
+  async update(){
+    this.stream.rtc.client = AgoraRTC.createClient({ mode: 'live', codec: 'vp8' });
 
+  await this.stream.rtc.client.setClientRole('audience');
+  }
   async startCall() {
       const uid = this.api.generateUid();
       const rtcDetails = await this.api.generateTokenAndUid(uid);
