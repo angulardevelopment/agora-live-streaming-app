00692b46ac3627b99bce790a53f7ab8ed4652ed1
Gupta, Sunny <sunnyvales789@gmail.com>
2025-07-12 22:12:00 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CallComponent } from './call/call.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { LiveComponent } from './live/live.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    CallComponent,
-    LiveComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        CallComponent,
+        LiveComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
